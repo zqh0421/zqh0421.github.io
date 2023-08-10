2bd9181e4eb550e30b96f29fdeacce4686d3f1dd
zqh0421 <zqh0421@hotmail.com>
2023-08-10 02:53:51 +0000

    Initialize astro

--- src/components/DownloadCV.tsx
+++
@@ -1,9 +0,0 @@
-const DownloadCV = (props: { className?: string}) => {
-  return (
-    <div className={`downloadCV ${props.className}`}>
-      <button className="border-transparent border-solid border-[1px] px-6 py-3 cursor-pointer transition-colors hover:border-[#646cff] bg-base-200" ><a target="_blank" href="https://drive.google.com/file/d/1h_XRQjizTTwRxbRRHvxCuOTYPoCwic8C/view?usp=share_link">Download Qianhui's Resume</a></button>
-    </div>
-  )
-}
-
-export default DownloadCV;
