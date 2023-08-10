e9ab4c088aa2f7422aba61df866cb304948c79ca
zqh0421 <zqh0421@hotmail.com>
2023-08-10 16:32:53 +0000

    Update LoadingScreen, NavBar

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,30 @@
+import useLockBody from '../hooks/useBodyLock';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+
+function LoadingScreen() {
+  const [isLoading, setIsLoading] = useState(true);
+  useLockBody(isLoading);
+
+  useEffect(() => {
+    setIsLoading(false);
+    document.body.classList.remove('fixed');
+  }, []);
+
+  return (
+    <AnimatePresence>
+      {isLoading && (
+        <motion.div
+          animate={{ y: '0%' }}
+          exit={{ y: '-100%' }}
+          transition={{ duration: 1, delay: 0.50 }}
+          className="fixed inset-0 z-50 flex items-center justify-center bg-neutrals-900"
+        >
+          <div className='bg-red-100 w-screen h-screen flex items-center justify-center text-4xl'>WELCOME!</div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
+
+export default LoadingScreen;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,49 +1,100 @@
 import useScroll from '../hooks/useScroll';
 import useWindowSize from '../hooks/useWindowSize';
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
+import Wrapper from '../ui/Wrapper';
+import { cn } from '../utils/cn';
+import MobileNavigation from './MobileNav';
 
-const links = [
-  {
-    label: 'About',
-    href: '/#about',
-  },
-  {
-    label: 'Work',
-    href: '/#work',
-  },
+export const menuItems = [
+	{
+		title: 'About',
+		path: '#about',
+	},
+	{
+		title: 'Projects',
+		path: '#projects',
+	},
+	{
+		title: 'Contact',
+		path: '#contact',
+	},
 ];
 
-interface HeaderProps {
+
+interface NavBarProps {
   isInitialBackgroundTransparent?: boolean | undefined;
 }
 
-function Header({ isInitialBackgroundTransparent = false }: HeaderProps) {
+function NavBarReact({ isInitialBackgroundTransparent = false }: NavBarProps) {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const windowSize = useWindowSize();
   const { scrollY } = useScroll();
-  console.log(scrollY, windowSize.height)
 
   const hasScrolledPastHeroSection = scrollY > windowSize.height - 1;
-  const isBackgroundShown =
-    // !isInitialBackgroundTransparent ||
-    hasScrolledPastHeroSection ||
-    isMobileMenuOpen;
-  console.log(isInitialBackgroundTransparent, hasScrolledPastHeroSection, isMobileMenuOpen)
 
+  const isBackgroundShown =
+    !isInitialBackgroundTransparent ||
+    hasScrolledPastHeroSection ||
+    isMobileMenuOpen
+  
   return (
-    <header
+    <nav
       aria-label="Primary"
-      className="fixed top-0 z-40 w-full"
+      className="fixed top-0 w-full z-40"
     >
       <div
-        className={`border-b-0.5 py-2 transition-colors duration-500 ${isBackgroundShown
+        className={cn('border-b-[0.5px] py-2 transition-colors duration-500',
+            isBackgroundShown
             ? 'border-neutral-600 bg-neutral-900/90 backdrop-blur-md supports-[backdrop-filter]:bg-neutral-900/50'
-            : 'border-transparent bg-transparent'}`}
+            : 'border-transparent bg-transparent')}
       >
-        123
+        <Wrapper className="flex flex-col items-center justify-between lg:flex-row">
+        <div className="grid grid-cols-3">
+          <div className="flex items-center md:hidden">
+            <MobileNavigation.Toggle
+              isOpen={isMobileMenuOpen}
+              onIsOpenChange={setIsMobileMenuOpen}
+            />
+          </div>
+          <nav
+            aria-label="Primary"
+            className="hidden items-center gap-x-6 md:flex"
+          >
+              {menuItems.map((item, index) => (
+                <a
+                  key={index}
+                  href={item.path}
+                  className="relative py-1 text-sm uppercase text-neutrals-50 after:absolute after:inset-x-0 after:bottom-0 after:h-px after:scale-x-0 after:bg-gradient-to-r after:from-transparent after:via-neutrals-200 after:to-transparent after:transition-transform hover:after:-scale-x-125 focus-visible:after:-scale-x-125"
+                >
+                  {item.title}
+                </a>
+              ))}
+            </nav>
+            <div className="flex items-center justify-center">
+              <a
+                href="/"
+                title="Navigate home"
+              >
+                <div className="h-3 md:h-4">Logo</div>
+              </a>
+            </div>
+            <div className="flex items-center justify-end">
+              Right
+            </div>
+          </div>
+        </Wrapper>
       </div>
-    </header>
+      <MobileNavigation
+          isOpen={isMobileMenuOpen}
+          onClose={() => setIsMobileMenuOpen(false)}
+          items={menuItems}
+        />
+        <MobileNavigation.Overlay
+          isOpen={isMobileMenuOpen}
+          onClose={() => setIsMobileMenuOpen(false)}
+        />
+    </nav>
   );
 }
 
-export default Header;
+export default NavBarReact;
