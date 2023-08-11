841aa4b1b8044ab8328073539615707fdd85a6d7
zqh0421 <zqh0421@hotmail.com>
2023-08-11 01:21:25 +0000

    Update vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), sitemap()],
-  experimental: { assets: true },
+  experimental: {
+    assets: true
+  },
+  output: "server",
+  adapter: vercel()
 });
