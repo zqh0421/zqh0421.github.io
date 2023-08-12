4ca3aad1465921bfe6307d863313b74a343d933e
zqh0421 <zqh0421@users.noreply.github.com>
2023-08-12 20:21:11 +0000

    deploy: 687718a02c82001eba0b89a46608776901cd9dcf

---
+++ _astro/resolve-element.f8ac943f.js
@@ -0,0 +1,1 @@
+import{i as a}from"./motion.86f98872.js";function v(r,o,i){var f;if(typeof r=="string"){let u=document;o&&(a(!!o.current),u=o.current),i?((f=i[r])!==null&&f!==void 0||(i[r]=u.querySelectorAll(r)),r=i[r]):r=u.querySelectorAll(r)}else r instanceof Element&&(r=[r]);return Array.from(r||[])}export{v as r};
