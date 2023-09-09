651d2661197c87a6cbf245148d3ece9a31cdf024
zqh0421 <zqh0421@hotmail.com>
2023-09-09 14:43:22 +0000

    Update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ["Lato", "Inter", ...defaultTheme.fontFamily.sans],
 				colors: {
 					primary: '#6919FF',
 					success: '#00F090',
