f617ca4c2abc3abc1eaedff95a1ea656b3fdc70b
Qianhui Zhao <79447496+zqh0421@users.noreply.github.com>
2025-07-11 21:12:23 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -61,7 +61,7 @@ function Footer() {
           </small>
           <small className="text-xs/5 text-neutral-300">
             {/* Last Updated: {months[currentMonth]} {currentDay}, {currentYear} */}
-            Last Updated: July 09, 2025
+            Last Updated: July 11, 2025
           </small>
         </div>
       </Wrapper>
